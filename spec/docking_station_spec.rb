ad409c9911822dc5f30537833268072464203ce6
Adam Romano <manoadamro.imp@gmail.com>
2017-08-01 16:26:16 +0100

    improved test in docking_station_spec: now uses variable

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,11 @@ require 'docking_station'
 describe DockingStation do
   it "docking station should not be nil" do
 
+    # define a variable with type DockingStation
+    docking_station = DockingStation.new
+
     # check that DockingStation can be initialized properly,
     # by checking it is not nil
-	  expect(DockingStation.new).not_to eq(nil)
-
+	  expect(docking_station).not_to eq(nil)
   end
 end
