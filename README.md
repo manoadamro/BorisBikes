9008ea5bb730ae2634a5617c056aff3523964f6d
Adam Romano <manoadamro.imp@gmail.com>
2017-08-01 16:08:40 +0100

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Boris Bikes
 
-
+This is an application that will be able to tell if there is a bike in a docking station, check that the bike is working and release the bike if both conditions are true.
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,9 +4,9 @@ require 'docking_station'
 describe DockingStation do
   it "docking station should not be nil" do
 
-  # check that DockingStation can be initialized properly,
-  # by checking it is not nil
-	expect(DockingStation.new).not_to eq(nil)
+    # check that DockingStation can be initialized properly,
+    # by checking it is not nil
+	  expect(DockingStation.new).not_to eq(nil)
 
   end
 end
